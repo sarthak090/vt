67219cb151f03e3dd84fb9e33e348951688cf1a3
sarthak <sarthakkaushik090@gmail.com>
2024-07-05 14:31:21 +0530

    post

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata, ResolvingMetadata } from 'next'
+ 
+type Props = {
+  params: { slug: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
 async function getPost(slug: string) {
   const res = await fetch(
     "https://wordpress-1294600-4703747.cloudwaysapps.com/wp-json/wp/v2/posts?slug=" +
@@ -13,6 +20,36 @@ async function getPost(slug: string) {
 
   return res.json();
 }
+ 
+export async function generateMetadata(
+    { params, searchParams }: Props,
+    parent: ResolvingMetadata
+  ): Promise<Metadata> {
+    // read route params
+    const slug = params.slug
+   
+    // fetch data
+    const res = await fetch(
+        "https://wordpress-1294600-4703747.cloudwaysapps.com/wp-json/wp/v2/posts?slug=" +
+          slug
+      );   
+      if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error("Failed to fetch data");
+      }
+    
+      const postData =await res.json();
+    //   console.log(postData)
+      const post = postData[0]
+   
+   
+    return {
+      title:post.title.rendered,
+      openGraph: {
+        images: ['/some-specific-page-image.jpg',],
+      },
+    }
+  }
 
 export default async function page({
   params: { slug },
